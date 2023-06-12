356a4a114ed2e37e20693d2ddce1613362b09ce2
Mukosi <mukosibudeli@gmail.com>
2023-06-12 08:21:28 +0200

    first commit

--- TableViews/ViewController.swift
+++ TableViews/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TableView.dataSource = self
+        TableView.delegate = self
     }
 
 
 }
+//MARK: - Datasource Protocol
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 07
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "I am a cell number : \(indexPath.row)"
+        
+        return cell
+    }
+}
 
+//MARK: - Delegates
+extension ViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        printContent("You have clicked row number : \(indexPath.row)")
+    }
+}
